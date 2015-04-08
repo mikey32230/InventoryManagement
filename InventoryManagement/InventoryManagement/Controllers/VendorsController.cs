868d27ef1beadfd045bb6409bcde404bce33a6c2
mikey32230 <mikey32230@gmail.com>
2015-04-08 16:25:38 -0400

    Initial Commit

---
+++ InventoryManagement/InventoryManagement/Controllers/VendorsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagement;
+using InventoryManagement.Models;
+
+namespace InventoryManagement.Controllers
+{
+    public class VendorsController : Controller
+    {
+        private InventoryEntities db = new InventoryEntities();
+
+        // GET: Vendors
+        public ActionResult Index()
+        {
+            return View(db.Vendors.ToList());
+        }
+
+        // GET: Vendors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vendor vendor = db.Vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // GET: Vendors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vendors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(VendorContactWrap venConWrap)
+        {
+            //I want to make the contact object, in it's entirety, optional.  
+            //This is currently impossible due to the validation implemented by Entity Framework?  
+            //Looking for a solution that doesn't require a lot of code duplication (i.e. recreating another contact class with different validation)
+            if (ModelState.IsValid)
+            {
+                var vendor = venConWrap.vendor;
+                if (venConWrap.contact != null)
+                {
+                    var contact = venConWrap.contact;
+                    vendor.Contacts.Add(contact);
+                }
+
+                db.Vendors.Add(vendor);
+                db.SaveChanges();
+
+                //contact.Vendor1 = vendor; 
+                //db.SaveChanges(); 
+                return RedirectToAction("Index");
+            }
+
+            return View(venConWrap);
+        }
+
+        // GET: Vendors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vendor vendor = db.Vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // POST: Vendors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vendor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vendor);
+        }
+
+        // GET: Vendors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vendor vendor = db.Vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // POST: Vendors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vendor vendor = db.Vendors.Find(id);
+            db.Vendors.Remove(vendor);  //How do we set a cascasde delete!!!! This will error currently
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Models/VendorContactWrap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace InventoryManagement.Models
+{
+    public class VendorContactWrap
+    {
+        public Vendor vendor { get; set; }   //Should I be exposing these directly to the view!!??? 
+        public Contact contact { get; set; } //Should I be exposing these directly to the view!!??? 
+
+
+    }
+}
