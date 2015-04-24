47151917b67dc7b4a25878219662d908028b34e7
MidnightRider13 <markowells@gmail.com>
2015-04-24 12:38:19 -0400

    Fixes to poor merge
    Corrected an accidental discard of AssetModel decorations.
    Cleaned up the csproj so that the project can actually load  0_0

--- InventoryManagement/InventoryManagement/AssetModel.cs
+++ InventoryManagement/InventoryManagement/AssetModel.cs
@@ -6,24 +6,32 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryManagement
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Web.Mvc;
+
     public partial class AssetModel
     {
+        public int Id { get; set; }
+
         public AssetModel()
         {
             this.Assets = new HashSet<Asset>();
         }
-    
-        public int Id { get; set; }
+
+        [Display(Name = "Model")]
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
+        [Display(Name = "Asset Type")]
+        [Required]
         public Nullable<int> TypeId { get; set; }
-    
+
         public virtual ICollection<Asset> Assets { get; set; }
         public virtual AssetType AssetType { get; set; }
     }
