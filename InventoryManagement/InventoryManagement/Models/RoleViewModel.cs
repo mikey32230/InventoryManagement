d6d78f7a43637b84679fb16921a8064ba3746e06
deannamika <griffith.703@osu.edu>
2015-04-25 16:21:42 -0400

    admin crud
    almost there

---
+++ InventoryManagement/InventoryManagement/Models/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class RoleViewModel
+    {
+        public String RoleId;
+        public String UserId; 
+    }
+}
